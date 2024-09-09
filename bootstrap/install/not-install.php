cf70e0744055adb0a1f22626e769a91e79e426f5
sts-sys <sts.framework@gmail.com>
2024-09-09 17:18:11 +0200

    Update not-install.php

--- bootstrap/install/not-install.php
+++ bootstrap/install/not-install.php
@@ -1,5 +1,4 @@
 <?php
-
 $images = 'http://' . $_SERVER['HTTP_HOST'] . '/public/images/logo.png';
 $favicon = 'http://' . $_SERVER['HTTP_HOST'] . '/public/favicon.png';
 echo <<<HTML
@@ -13,21 +12,32 @@ echo <<<HTML
         
         <div class="error">
         <!--  <h1 class="logo-text"><span class="logo-1">STS</span> <span class="logo-2">Framework!</span></h1> -->
-        <h2 class="notice">Important Notice</h2>
         <!-- <p>This is a sample error message. Please replace it with the actual error message from your application.</p> -->
         <!-- <p>Please contact the system administrator or developer for further assistance.</p> -->
-        <p>Your STS Framework is a powerful, lightweight, and flexible PHP framework that aims to simplify the development process for web applications.</p>
-        <p>For more information, please visit <a href="https://github.com/sts-sys/sts" class="link">our GitHub repository</a>.</p>
+        <p style="padding: 0 15px">Your application is a flexible and modular web application designed to be highly customizable and efficient. It includes a robust theming system that allows users to manage different themes, each with its own assets, layouts, partials, views, and translations. Themes are stored in a dedicated directory, and the application supports multiple themes that can be dynamically switched or customized.</p>
+        <p style="padding: 0 15px">The application leverages traits and directives for template management, allowing for complex templates with custom directives for error handling, dynamic content blocks, SEO optimization, form validation, and more. This structure provides flexibility and scalability, making it easy to extend and maintain.<br />Overall, your application aims to provide a powerful and user-friendly platform for developers to build, customize, and manage dynamic web content efficiently.</p>
+        <hr>
+        <h2 class="notice">Important Notice</h2>
         <p>To get started, please follow the <a href="https://github.com/sts-sys/sts?tab=readme-ov-file#install" class="link">installation instructions</a>.</p>
+        <p>For help with your project, please join our <a href="https://github.com/sts-sys/sts" class="link">GitHub repository</a>.</p>
+        <p>For more information, please visit <a href="https://github.com/sts-sys/sts/wiki" class="link">our documentation</a>.</p>
         </div>
-
+        <div style="text-align: center;"> 
+        <span class="link" style="text-decoration: none;">&copy; 2024 - STS Solutions. All right reserved.</span> | <a style="text-decoration: none;" href="https://github.com/sts-sys/sts/issues" class="link">Report an Issue</a> | <a style="text-decoration: none;" href="https://github.com/sts-sys/sts/wiki" class="link">Learn More</a>
+        </div>
         <style type="text/css">
             span.logo-1 {
                 color: #336A86;
+                text-shadow: 0px 1px 0px #00000061;
+            }
+
+            hr {
+                border: 1px solid #6588bd21;
             }
 
             span.logo-2 {
                 color: #8EAEC4;
+                text-shadow: 0px 1px 0px #00000061;
             }
 
             h2.notice {
@@ -48,15 +58,21 @@ echo <<<HTML
                 text-align: center;
                 white-space: normal;
                 font-size: 16px;
-                background: #457a9314;
+                background: transparent;
                 padding: 10px 0;
                 color: #5e5e5e;
                 font-family: math;
                 font-weight: 600;
                 border-radius: 3px;
-                box-shadow: 0px 2px 3px #00000021;
+                border-bottom: 3px solid #3e3a3a14;
+                box-shadow: 0 8px 4px #3e3a3a12;
             }
 
+            .error > p {
+                padding: 0 5px;
+                margin: 4px auto;
+            }
+            
             .logo {
                 display: block;
                 margin: 20px auto;
